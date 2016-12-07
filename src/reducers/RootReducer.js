a8693322c144e4ec5938a1381ff34ced66b7a1ff
Andrew Crist <andrew.l.crist@gmail.com>
2016-12-06 21:31:57 -0700

    UPDATE REDUCERS

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux'
 import LocalWeatherReducer from './LocalWeatherReducer'
-import PinnedWeatherReducer from './PinnedWeatherReducer'
 import PinLocation from './PinLocation'
 
 const RootReducer = combineReducers({
 	LocalWeatherReducer,
-	PinnedWeatherReducer,
 	PinLocation
 });
 
